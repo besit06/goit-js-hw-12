09e5e359c6f237b542897adb40f2542a31c79c83
besit06 <behemoth2054@gmail.com>
2024-09-27 13:06:23 +0200

    js12

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import { fetchImages } from './js/pixabay-api.js';
-import { renderImages, showErrorMessage, appendImages } from './js/render-functions.js';
+import { renderImages, appendImages, showErrorMessage } from './js/render-functions.js';
 
 const form = document.querySelector('.search-form');
 const input = form.querySelector('input[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
 const loader = document.getElementById('loader');
-const loadMoreButton = document.createElement('button');
-loadMoreButton.textContent = 'Load more';
-loadMoreButton.classList.add('btn', 'load-more');
-document.body.appendChild(loadMoreButton);
-loadMoreButton.style.display = 'none';
+const loadMoreButton = document.querySelector('.load-more');
 
 let currentPage = 1;
 const perPage = 15;
 let query = '';
+
+
+
 
 function showLoader() {
   loader.classList.remove('hidden');
@@ -26,7 +25,7 @@ function hideLoader() {
 form.addEventListener('submit', onSearch);
 loadMoreButton.addEventListener('click', onLoadMore);
 
-function onSearch(event) {
+async function onSearch(event) {
   event.preventDefault();
 
   query = input.value.trim();
@@ -38,48 +37,43 @@ function onSearch(event) {
     return;
   }
 
-  loadImages(query, currentPage);
+  await loadImages(query, currentPage);
 }
 
-function onLoadMore() {
+async function onLoadMore() {
   currentPage++;
-  loadImages(query, currentPage);
+  await loadImages(query, currentPage);
 }
 
-function loadImages(query, page) {
-  showLoader();
-  loadMoreButton.style.display = 'none'; 
+async function loadImages(query, page) {
+  try {
+    showLoader();
+    
+    loadMoreButton.classList.add('hidden'); 
+    
 
-  fetchImages(query, page, perPage)
-    .then(data => {
-      hideLoader();
+    const data = await fetchImages(query, page, perPage);
 
-      if (data.hits.length === 0 && page === 1) {
-        showErrorMessage('Sorry, there are no images matching your search query. Please try again!');
-        return;
-      }
+    hideLoader(); 
+    
+    
+    if (data.hits.length === 0 && page === 1) {
+      showErrorMessage('Sorry, there are no images matching your search query. Please try again!');
+      return;
+    }
 
-      appendImages(data.hits); 
+    appendImages(data.hits);
+    
+    
+    if (data.hits.length < perPage || data.totalHits <= page * perPage) {
+      showErrorMessage("We're sorry, but you've reached the end of search results.");
+    } else {
+      loadMoreButton.classList.remove('hidden'); 
+      
+    }
 
-      if (data.hits.length < perPage || data.totalHits <= page * perPage) {
-        showErrorMessage("We're sorry, but you've reached the end of search results.");
-      } else {
-        loadMoreButton.style.display = 'block'; 
-      }
-
-      smoothScroll();
-    })
-    .catch(error => {
-      hideLoader();
-      showErrorMessage('Something went wrong. Please try again later.');
-      console.log(error)
-    });
-}
-
-function smoothScroll() {
-  const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
+  } catch (error) {
+    hideLoader();
+    showErrorMessage('Something went wrong. Please try again later.');
+  }
 }
